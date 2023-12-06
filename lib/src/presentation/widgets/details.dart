d7df5163b4f6f28c3176ec7ea18484db4b126b9c
hhualiang <hhualiang99@gmail.com>
2023-12-06 12:05:44 -0300

    Photo Animation

--- lib/src/presentation/widgets/details.dart
+++ lib/src/presentation/widgets/details.dart
@@ -3,13 +3,39 @@ import 'package:flutter/material.dart';
 import '../../domain/entity/detailed_recipe.dart';
 import 'expandable_list.dart';
 
-class Details extends StatelessWidget {
+class Details extends StatefulWidget {
   const Details({
     super.key,
     required this.recipe,
   });
 
   final FullRecipe recipe;
+
+  @override
+  State<Details> createState() => _DetailsState();
+}
+
+class _DetailsState extends State<Details> with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 3),
+    vsync: this,
+  );
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeInOutQuart,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.forward().whenComplete(() {});
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +45,7 @@ class Details extends StatelessWidget {
         child: Column(
           children: <Widget>[
             Text(
-              recipe.title,
+              widget.recipe.title,
               style: const TextStyle(
                 color: Colors.black,
                 fontSize: 24,
@@ -27,11 +53,16 @@ class Details extends StatelessWidget {
               ),
               textAlign: TextAlign.center, // Title Style
             ),
-            Image.network(
-              recipe.image, // Replace with your image url
-              height: 300,
-              width: double.infinity,
-              fit: BoxFit.cover,
+            SizeTransition(
+              sizeFactor: _animation,
+              axis: Axis.horizontal,
+              axisAlignment: -1,
+              child: Image.network(
+                widget.recipe.image, // Replace with your image url
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
             ),
             Container(
               padding: const EdgeInsets.all(16),
@@ -48,7 +79,7 @@ class Details extends StatelessWidget {
                       fontFamily: 'Roboto',
                     ), // Subtitle Style
                   ),
-                  ExpandableListView(items: recipe.extendedIngredients),
+                  ExpandableListView(items: widget.recipe.extendedIngredients),
                   const SizedBox(height: 16),
                   const Text(
                     'Instructions:',
@@ -59,7 +90,7 @@ class Details extends StatelessWidget {
                     ), // Subtitle Style
                   ),
                   Text(
-                    recipe.summary,
+                    widget.recipe.summary,
                     style: const TextStyle(
                       color: Colors.black,
                       fontSize: 16,
